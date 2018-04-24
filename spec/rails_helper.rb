dcaf58e9af87ba148f4a2f62448f5d44126329ce
Ravi Mehra <ravi.lib17@gmail.com>
2018-04-24 11:12:10 +0530

    all done

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   config.use_transactional_fixtures = true
 
   config.include FactoryBot::Syntax::Methods
